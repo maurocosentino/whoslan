046c94eefb4944c178f806b52051f7489e4f0358
maurocosentino <mauroxcosentino@gmail.com>
2026-07-04 15:45:13 -0300

    Run arp-scan as subprocess and print raw output

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("netwatch - starting up")
+	cmd := exec.Command("sudo", "arp-scan", "--interface=enp1s0", "--localnet")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error ejecutando arp-scan:", err)
+		fmt.Println(string(output))
+		return
+	}
+
+	fmt.Println(string(output))
 }
